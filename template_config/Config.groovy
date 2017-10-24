1d0f1a0c400f09d8124878eb7992fd119d24cf7e
Jakub J Jablonski <jakub.jablonski2@tomtom.com>
2017-10-24 19:55:14 +0200

    Remove embedded arc42 template and make docToolchain standalone tool
    This change removes the arc42 template from docToolchain.
    New tasks to optionally download arc42 template are provided.
    Documentation is updated accordingly.

--- template_config/Config.groovy
+++ template_config/Config.groovy
@@ -2,19 +2,17 @@
 // Path where the docToolchain will produce the output files.
 // This path is appended to the docDir property specified in gradle.properties
 // or in the command line, and therefore must be relative to it.
-outputPath = 'build/docs'
+outputPath = 'build'
 
 // Path where the docToolchain will search for the input files.
 // This path is appended to the docDir property specified in gradle.properties
 // or in the command line, and therefore must be relative to it.
-inputPath = 'src/docs'
+inputPath = '.'
 
-inputFiles = [[file: 'test.adoc',              formats: ['html','pdf','docbook']],
-              [file: 'manual.adoc',            formats: ['html']],
-              [file: 'ppt/Demo.pptx.ad',       formats: ['html','revealjs']],
-             ]
+inputFiles = [[file: 'arc42-template.adoc', formats: ['html','pdf','docbook']]]
 
-taskInputsDirs = ["${inputPath}/images",
+taskInputsDirs = ["${inputPath}/src",
+                  "${inputPath}/images",
                  ]
 
-taskInputsFiles = []
+taskInputsFiles = ["${inputPath}/arc42-template.adoc"]
