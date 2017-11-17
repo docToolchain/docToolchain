a2e272c009aa37a39332d5a40dda2c3941dbd2e2
Ralf D. Mueller <ralf.d.mueller@gmail.com>
2017-11-17 20:49:56 +0100

    refactored exportChangelog regarding mkdirs()

---
+++ src/test/groovy/docToolchain/ExportExcelSpec.groovy
@@ -0,0 +1,51 @@
+package docToolchain
+import spock.lang.*
+import org.gradle.testkit.runner.GradleRunner
+import static org.gradle.testkit.runner.TaskOutcome.*
+
+class ExportChangeLogSpec extends Specification {
+
+    def gradleCommand
+
+    void 'test creation of log file'() {
+        setup: 'clean the environment'
+        when: 'remove old changelog file'
+            new File('./build/docs/changelog.adoc').delete()
+        then: 'log file does not exist'
+            new File('./build/docs/changelog.adoc').exists() == false
+        when: 'the gradle task is invoked'
+            def result = GradleRunner.create()
+                    .withProjectDir(new File('.'))
+                    .withArguments(['exportChangeLog','--info'])
+                    .build()
+        then: 'the task has been successfully executed'
+            result.task(":exportChangeLog").outcome == SUCCESS
+        and: 'the log file has been created'
+            new File('./build/docs/changelog.adoc').exists() == true
+        and: 'its content ends with our sample file'
+            new File('./build/docs/changelog.adoc')
+                    .text.trim().replaceAll("\r","")
+                    .endsWith("""
+| 2017-09-08 
+| Isidoro 
+| Added spanish translation for Arc42 Template 
+
+| 2017-04-09 
+| Ralf D. Mueller 
+| fix #24 template updated to V7.0 
+
+| 2017-04-08 
+| Ralf D. Mueller 
+| fixed typo 
+
+| 2016-10-03 
+| Ralf D. Mueller 
+| added jira based open issues list 
+
+| 2016-08-21 
+| Ralf D. Mueller 
+| added arc42 template as content 
+""".trim().replaceAll("\r",""))
+    }
+
+}
