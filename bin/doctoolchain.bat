e5f677be825665149c621f609ab001f9d7cc5c2d
Ralf D. Mueller <ralf.d.mueller@gmail.com>
2018-01-24 08:21:33 +0100

    fix #131 doctoolchain.bat

--- bin/doctoolchain.bat
+++ bin/doctoolchain.bat
@@ -15,30 +15,38 @@ set BASEDIR=%SCRIPTDIR%..\
 set GRADLECMD=%BASEDIR%gradlew.bat
 set WORKINGDIR=%cd%\
 
-@rem Execute docToolchain
+@REM Execute docToolchain
 
-%GRADLECMD% --project-cache-dir %BASEDIR%.gradle -p %BASEDIR% -PdocDir=%WORKINGDIR%%1 %2 %3 %4 %5 %6 %7 %8 %9
+@REM %GRADLECMD% --project-cache-dir %BASEDIR%.gradle -p %BASEDIR% -PdocDir=%WORKINGDIR%%1 %2 %3 %4 %5 %6 %7 %8 %9
+
+cd %BASEDIR%
+
+./gradlew --project-cache-dir %BASEDIR%/.gradle -PdocDir=%WORKINGDIR%%1 %2 %3 %4 %5 %6
 
 goto :end
 
 :help
 
+
 echo Usage: doctoolchain docDir [option...] [task...]
-echo
+echo.
 echo docDir - Absolute directory with the documentation.
-echo
+echo.
 echo You can use the same options and tasks as in underlying gradle.
 echo Use "doctoolchain . --help" to see available options.
 echo Use "doctoolchain . tasks" to see available tasks.
-echo
+echo.
 echo Examples:
-echo
+echo.
+echo   Init new project with arc42 tempalte
+echo     doctoolchain . -b init.gradle initArc42EN
+echo.
 echo   Generate PDF:
 echo     doctoolchain . generatePDF
-echo
+echo.
 echo   Generate HTML:
 echo     doctoolchain . generateHTML
-echo
+echo.
 echo   Publish HTML to Confluence:
 echo     doctoolchain . publishToConfluence -PconfluenceConfigFile=ConfluenceConfig.groovy --no-daemon -q
 goto :end
