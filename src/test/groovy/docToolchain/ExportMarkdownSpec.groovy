e626c8256755a42c9eb4c4d47e9072ce6a58b84a
Fabian Nonnenmacher <fabian.nonnenmacher@web.de>
2018-07-11 22:52:54 +0200

    Add test to verify markdown conversion

---
+++ src/test/groovy/docToolchain/ExportMarkdownSpec.groovy
@@ -0,0 +1,33 @@
+package docToolchain
+
+import org.gradle.testkit.runner.GradleRunner
+import spock.lang.Specification
+
+import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
+
+class ExportMarkdownSpec extends Specification {
+
+
+    public static final File exportedMarkdownFile = new File('./build/docs/exportMarkdownDocs.adoc')
+
+    void 'test conversion of sample markdown file'() {
+
+        given: 'a clean the environment'
+        exportedMarkdownFile.delete()
+
+        when: 'executing the gradle task `exportMarkdown`'
+        def result = GradleRunner.create()
+                .withProjectDir(new File('.'))
+                .withArguments('exportMarkdown')
+                .build()
+
+        then: 'the task has been successfully executed'
+        result.task(":exportMarkdown").outcome == SUCCESS
+
+        and: 'the AsciiDoc file has been created'
+        exportedMarkdownFile.exists()
+
+        and: 'its content is in AsciiDoc syntax'
+        exportedMarkdownFile.text.startsWith('= exportMarkdown')
+    }
+}
