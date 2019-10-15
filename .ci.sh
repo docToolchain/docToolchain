bb69593b3e9aab0913f34c5b9055ceac804e3a44
Daniel Bast <2790401+dbast@users.noreply.github.com>
2019-10-15 10:34:15 +0200

    Add extendable CI script which runs via CI and locally

---
+++ .ci.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+set -o errtrace -o nounset -o pipefail -o errexit
+
+# Goto directory of this script
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+cleaning () {
+  echo "############################################"
+  echo "#                                          #"
+  echo "#        Cleaning                          #"
+  echo "#                                          #"
+  echo "############################################"
+  ./gradlew clean
+}
+
+dependency_info() {
+  echo "############################################"
+  echo "#                                          #"
+  echo "#        Check for dependency updates      #"
+  echo "#                                          #"
+  echo "############################################"
+  ./gradlew dependencyUpdates
+}
+
+unit_tests () {
+  echo "############################################"
+  echo "#                                          #"
+  echo "#        Unit testing                      #"
+  echo "#                                          #"
+  echo "############################################"
+  ./gradlew test --info
+}
+
+integration_tests () {
+  echo "############################################"
+  echo "#                                          #"
+  echo "#        Integration testing               #"
+  echo "#                                          #"
+  echo "############################################"
+}
+
+check_for_clean_worktree() {
+  echo "############################################"
+  echo "#                                          #"
+  echo "#        Check for clean worktree          #"
+  echo "#                                          #"
+  echo "############################################"
+  # To be executed after all other steps, to ensures that there is no
+  # uncommitted code and there are no untracked files, which means .gitignore is
+  # complete and all code is part of a reviewable commit.
+  GIT_STATUS="$(git status --porcelain)"
+  if [[ $GIT_STATUS ]]; then
+    echo "Your worktree is not clean, there is either uncommitted code or there are untracked files:"
+    echo "${GIT_STATUS}"
+    exit 1
+  fi
+}
+
+cleaning
+dependency_info
+unit_tests
+#integration_tests
+check_for_clean_worktree
