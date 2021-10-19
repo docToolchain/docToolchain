57c3d89e75a7e6734f7a6da66d5e71035787397c
Gerd Aschemann <gerd@aschemann.net>
2021-10-19 08:32:06 +0200

    Introduce GH Action default build (#667)

--- .ci.sh
+++ .ci.sh
@@ -2,6 +2,30 @@
 
 set -o errtrace -o nounset -o pipefail -o errexit
 
+# Enable build on Travis as well as on GH Actions
+set +u
+if test "${GITHUB_WORKFLOW}"; then
+    BRANCH=${GITHUB_HEAD_REF}
+    BUILD_DIR=${GITHUB_WORKSPACE}
+    BUILD_NUMBER="${GITHUB_WORKFLOW}-${GITHUB_RUN_ID}-${GITHUB_RUN_NUMBER}"
+    CI_SERVER="Github"
+    PULL_REQUEST=$(test "${GITHUB_HEAD_REF}" && echo "true" || echo "false")
+    # JDK_VERSION is set by GH Action
+    # RUNNER_OS is set by GH Action
+elif test "${TRAVIS_BRANCH}"; then
+    BRANCH=${TRAVIS_BRANCH}
+    BUILD_NUMBER=${TRAVIS_BUILD_NUMBER}
+    BUILD_DIR=${TRAVIS_BUILD_DIR}
+    CI_SERVER="Travis"
+    PULL_REQUEST=${TRAVIS_PULL_REQUEST:-"false"}
+    JDK_VERSION=${TRAVIS_JDK_VERSION}
+    RUNNER_OS="ubuntu-latest"
+else
+    echo "Cannot determine CI Server (Travis or Github)" >&2
+    exit 1
+fi
+set -u
+
 # Goto directory of this script
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
@@ -30,7 +54,7 @@ unit_tests () {
   echo "#        Unit testing                      #"
   echo "#                                          #"
   echo "############################################"
-  if [ "$TRAVIS_BRANCH" == "ng" ] || [ "$TRAVIS_BRANCH" == "main-2.x" ] ; then
+  if [ "${BRANCH}" == "ng" ] || [ "${BRANCH}" == "main-2.x" ] ; then
     echo "skipping tests for now"
   else
     ./gradlew test --info
@@ -43,7 +67,7 @@ integration_tests () {
   echo "#        Integration testing               #"
   echo "#                                          #"
   echo "############################################"
-  if [ "$TRAVIS_BRANCH" == "ng" ] || [ "$TRAVIS_BRANCH" == "main-2.x" ] ; then
+  if [ "${BRANCH}" == "ng" ] || [ "${BRANCH}" == "main-2.x" ] ; then
     echo "skipping tests for now"
   else
       TEMPLATES='Arc42DE Arc42EN Arc42ES'
@@ -74,7 +98,7 @@ check_for_clean_worktree() {
   # uncommitted code and there are no untracked files, which means .gitignore is
   # complete and all code is part of a reviewable commit.
   GIT_STATUS="$(git status --porcelain)"
-  if [[ $GIT_STATUS ]]; then
+  if [[ ${GIT_STATUS} ]]; then
     echo "Your worktree is not clean, there is either uncommitted code or there are untracked files:"
     echo "${GIT_STATUS}"
     exit 1
@@ -87,8 +111,8 @@ create_doc () {
   echo "#        Create documentation              #"
   echo "#                                          #"
   echo "############################################"
-  echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
-  if [ "$TRAVIS_BRANCH" == "ng" ] || [ "$TRAVIS_BRANCH" == "main-2.x" ] ; then
+  echo "TRAVIS_BRANCH=${BRANCH}"
+  if [ "${BRANCH}" == "ng" ] || [ "${BRANCH}" == "main-2.x" ] ; then
     echo ">>> exportMarkdown"
     ./dtcw local exportMarkdown
     echo ">>> exportChangelog"
@@ -111,7 +135,7 @@ publish_doc () {
   echo "publish_doc"
   # Take from and modified http://sleepycoders.blogspot.de/2013/03/sharing-travis-ci-generated-files.html
   # ensure publishing doesn't run on pull requests, only when token is available and only on JDK11 matrix build and on master or a travisci test branch
-  if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ -n "$GH_TOKEN" ] && [ "$TRAVIS_JDK_VERSION" == "openjdk11" ] && { [ "$TRAVIS_BRANCH" == "travisci" ] || [ "$TRAVIS_BRANCH" == "master" ] || [ "$TRAVIS_BRANCH" == "ng" ] || [ "$TRAVIS_BRANCH" == "main-1.x" ] || [ "$TRAVIS_BRANCH" == "main-2.x" ]; } ; then
+  if [ "${PULL_REQUEST}" == "false" ] && [ -n "${GH_TOKEN}" ] && { [ "${JDK_VERSION}" == "openjdk11" ] || { [ "${JDK_VERSION}" == "11-adopt" ] && [ "${RUNNER_OS}" == "ubuntu-latest" ]; }; } && { [ "${BRANCH}" == "travisci" ] || [ "${BRANCH}" == "master" ] || [ "${BRANCH}" == "ng" ] || [ "${BRANCH}" == "main-1.x" ] || [ "${BRANCH}" == "main-2.x" ]; } ; then
     echo "############################################"
     echo "#                                          #"
     echo "#        Publish documentation             #"
@@ -120,29 +144,29 @@ publish_doc () {
     echo -e "Starting to update gh-pages\n"
 
     #go to home and setup git
-    cd "$HOME"
+    cd "${HOME}"
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "Travis"
 
     #using token clone gh-pages branch
     git clone --quiet --branch=gh-pages "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" gh-pages > /dev/null
 
-    if [ "$TRAVIS_BRANCH" == "master" ] || [ "$TRAVIS_BRANCH" == "main-1.x" ] ; then
+    if [ "${BRANCH}" == "master" ] || [ "${BRANCH}" == "main-1.x" ] ; then
       #go into directory and copy data we're interested in to that directory
       cd gh-pages
       rm -rf v1.3.x/*
-      cp -Rf "$TRAVIS_BUILD_DIR"/docs/* v1.3.x/.
+      cp -Rf "${BUILD_DIR}"/docs/* v1.3.x/.
     fi
-    if [ "$TRAVIS_BRANCH" == "ng" ] || [ "$TRAVIS_BRANCH" == "main-2.x" ] ; then
+    if [ "${BRANCH}" == "ng" ] || [ "${BRANCH}" == "main-2.x" ] ; then
       #go into directory and copy data we're interested in to that directory
       cd gh-pages
       rm -rf v2.0.x/*
-      cp -Rf "$TRAVIS_BUILD_DIR"/build/microsite/output/* v2.0.x/.
+      cp -Rf "${BUILD_DIR}"/build/microsite/output/* v2.0.x/.
     fi
 
     #add, commit and push files
     git add -f .
-    git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+    git commit -m "${CI_SERVER} build '${BUILD_NUMBER}' pushed to gh-pages"
     git push -fq origin gh-pages > /dev/null
 
     echo -e "Done publishing to gh-pages.\n"
