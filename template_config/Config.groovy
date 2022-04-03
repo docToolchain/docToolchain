90ccea1314f636f0031b2766d7559db501e47cdd
Ralf D. Müller <ralf.d.mueller@gmail.com>
2022-04-03 21:34:11 +0200

    changed previewPort to same as container #834

--- template_config/Config.groovy
+++ template_config/Config.groovy
@@ -47,7 +47,7 @@ microsite.with {
     // is your microsite deployed with a context path?
     contextPath = '/'
     // configure a port on which your preview server will run
-    previewPort = 8881
+    previewPort = 8042
     // the folder of a site definition (theme) relative to the docDir+inputPath
     //siteFolder = '../site'
 
