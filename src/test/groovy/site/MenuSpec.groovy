de10e7b03645e0eed0c5cd5429ede6f97c2b0d02
Jeremie Bresson <jeremie.bresson@unblu.com>
2022-07-08 07:55:06 +0200

    Menu configuration (improve documentation and test)

--- src/test/groovy/site/MenuSpec.groovy
+++ src/test/groovy/site/MenuSpec.groovy
@@ -9,11 +9,14 @@ class MenuSpec extends Specification {
         def menu
         def entriesMap
         def newEntries
+        def rootpath = '/root_path/'
+        def uri = 'content_uri/'
     }
 
-    void 'test empty published content'() {
+    void 'test empty published content and empty config'() {
         given: 'empty published_content'
             Binding binding = new Binding()
+            binding.config = [:]
             binding.published_content = []
         when: 'run the `menu.groovy` script'
             runMenuScript(binding)
@@ -24,27 +27,79 @@ class MenuSpec extends Specification {
     }
 
     void 'test with published content'() {
-        given: '3 pages in published_content'
+        given: '3 pages in published_content and empty menu config'
             Binding binding = new Binding()
+            binding.config = [site_menu: [:]]
             binding.published_content = [
-                ['jbake-menu': 'foo', 'jbake-title': 'Lorem Ipsum', 'jbake-order': 10, uri : 'foo/10_lorem-ipsum.html'],
-                ['jbake-menu': 'foo', 'jbake-title': 'Dolor sit amet', 'jbake-order': 20, uri : 'foo/20_dolor_sit_amet.html'],
-                ['jbake-menu': 'bar', 'jbake-title': 'Adipiscing elit', 'jbake-order': 10, uri : 'bar/10_adipiscing_elit.html']
+                ['jbake-menu': 'foo', 'jbake-title': 'Lorem Ipsum', 'jbake-order': '10', uri : 'foo/10_lorem-ipsum.html'],
+                ['jbake-menu': 'foo', 'jbake-title': 'Dolor sit amet', 'jbake-order': '20', uri : 'foo/20_dolor_sit_amet.html'],
+                ['jbake-menu': 'bar', 'jbake-title': 'Adipiscing elit', 'jbake-order': '10', uri : 'bar/10_adipiscing_elit.html']
             ]
         when: 'run the `menu.groovy` script'
             runMenuScript(binding)
         then: 'arrays are computed'
             binding.content.menu == [
                 foo:[
-                    [title: 'Lorem Ipsum', order: 10, filename: null, uri: 'foo/10_lorem-ipsum.html'],
-                    [title: 'Dolor sit amet', order: 20, filename: null, uri: 'foo/20_dolor_sit_amet.html']
+                    [title: 'Lorem Ipsum', order: '10', filename: null, uri: 'foo/10_lorem-ipsum.html'],
+                    [title: 'Dolor sit amet', order: '20', filename: null, uri: 'foo/20_dolor_sit_amet.html']
                 ],
                 bar:[
-                    [title: 'Adipiscing elit', order: 10, filename: null, uri: 'bar/10_adipiscing_elit.html']
+                    [title: 'Adipiscing elit', order: '10', filename: null, uri: 'bar/10_adipiscing_elit.html']
                 ]
             ]
-            binding.content.entriesMap == [:]
-            binding.content.newEntries == []
+            binding.content.entriesMap ==  [
+                foo:[ 'foo', [
+                        [title: 'Lorem Ipsum', order: '10', filename: null, uri: 'foo/10_lorem-ipsum.html'],
+                        [title: 'Dolor sit amet', order: '20', filename: null, uri: 'foo/20_dolor_sit_amet.html']
+                    ]
+                ],
+                bar:[ 'bar', [
+                        [title: 'Adipiscing elit', order: '10', filename: null, uri: 'bar/10_adipiscing_elit.html']
+                    ]
+                ]
+            ]
+            binding.content.newEntries == [
+                [ isActive: '', href: '/root_path/foo/10_lorem-ipsum.html', title: 'foo'],
+                [ isActive: '', href: '/root_path/bar/10_adipiscing_elit.html', title: 'bar']
+            ]
+    }
+
+    void 'test with published content and menu config'() {
+        given: '3 pages in published_content and menu config'
+            Binding binding = new Binding()
+            binding.config = [site_menu: [code1: 'title1', code2: 'title2']]
+            binding.published_content = [
+                ['jbake-menu': 'code1', 'jbake-title': 'Lorem Ipsum', 'jbake-order': '10', uri : 'pages/10_lorem-ipsum.html'],
+                ['jbake-menu': 'code1', 'jbake-title': 'Dolor sit amet', 'jbake-order': '20', uri : 'pages/20_dolor_sit_amet.html'],
+                ['jbake-menu': 'code2', 'jbake-title': 'Adipiscing elit', 'jbake-order': '30', uri : 'pages/30_adipiscing_elit.html']
+            ]
+        when: 'run the `menu.groovy` script'
+            runMenuScript(binding)
+        then: 'arrays are computed'
+            binding.content.menu == [
+                code1:[
+                    [title: 'Lorem Ipsum', order: '10', filename: null, uri: 'pages/10_lorem-ipsum.html'],
+                    [title: 'Dolor sit amet', order: '20', filename: null, uri: 'pages/20_dolor_sit_amet.html']
+                ],
+                code2:[
+                    [title: 'Adipiscing elit', order: '30', filename: null, uri: 'pages/30_adipiscing_elit.html']
+                ]
+            ]
+            binding.content.entriesMap ==  [
+                code1:[ 'title1', [
+                        [title: 'Lorem Ipsum', order: '10', filename: null, uri: 'pages/10_lorem-ipsum.html'],
+                        [title: 'Dolor sit amet', order: '20', filename: null, uri: 'pages/20_dolor_sit_amet.html']
+                    ]
+                ],
+                code2:[ 'title2', [
+                        [title: 'Adipiscing elit', order: '30', filename: null, uri: 'pages/30_adipiscing_elit.html']
+                    ]
+                ]
+            ]
+            binding.content.newEntries == [
+                [ isActive: '', href: '/root_path/pages/10_lorem-ipsum.html', title: 'title1'],
+                [ isActive: '', href: '/root_path/pages/30_adipiscing_elit.html', title: 'title2']
+            ]
     }
 
     def runMenuScript(Binding binding) {
