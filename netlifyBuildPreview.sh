c370955b4a741df3680ff00cfe0be663bd8e7f3e
Ralf D. Müller <ralf.d.mueller@gmail.com>
2022-07-25 15:20:51 +0200

    #876 netlify build

---
+++ netlifyBuildPreview.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# create a locally installed version of the current repo
+mkdir -p $HOME/.doctoolchain/docToolchain-dev
+cp -r * $HOME/.doctoolchain/docToolchain-dev/.
+# set 'dev' as version in dtcw
+sed -i 's/VERSION=[-0-9.a-z]*/VERSION=dev/' dtcw
+# ensure that dtcw is installed
+./dtcw tasks
+# export information about contributors for those little avatars on top of each file
+./dtcw exportContributors
+# for the excel demo to work, we need to export the excel file
+./dtcw exportExcel
+# the docs for exportMarkdown are written in markdown, so let's export them
+./dtcw exportMarkdown
+# and now we are ready to generate the site
+./dtcw generateSite
